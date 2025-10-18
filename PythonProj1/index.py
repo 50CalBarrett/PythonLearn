4b84e5b4603a86852963dc64335beea0a2906d49
50CalBarrett <alexander.barrett.f@gmail.com>
2025-10-18 08:38:11 -0500

    Updates to Doc
    More learning initiatives.

---
+++ PythonProj1/index.py
@@ -0,0 +1,14 @@
+class jobPost:
+    def __init__(self, title, description, requirements,salary):
+        self.title = title
+        self.description = descr
+        self.requirements = req
+        self.salary = salary
+
+    #INSTANCE 
+    def get_info(self):
+        with open("/Users/alexander/Documents",r):
+	        file_stuff = File1.read()
+	        print(file_stuff)
+	        print(File1.closed)
+	        print(file_stuff)
